3b9a3f31d3fbe8173ed8453ff008bfe693ef0627
caizixiang <czxplp@gmail.com>
2018-11-29 00:07:48 +0800

    First prediction

--- main.py
+++ main.py
@@ -20,40 +20,48 @@ if __name__ == '__main__':
     flow_train = pd.read_csv('../data/flow_train.csv')
     transition_train = pd.read_csv('../data/transition_train.csv')
 
-    #We'll start by playing the flow, then the transition
-    ###statistic with mod7, week
-    flow_sample = pd.read_csv('../data/flow_train_sample.csv')
-    print(flow_sample.head())
-    # flow_sample.info()
+    #read all sample path
+    sample_data_path = '../data/flow/'
+    all_sample = os.listdir(sample_data_path)
 
-    ##sample
-    #group by mod7
-    sample_result_mod_7 = stat_mod_n(7, flow_sample)
-    # print(result_sample_by_mod7)
+    result_for_each_sample = []
+    for sample in tqdm(all_sample):
+        city, district = sample[:-4].split('_')
+        #We'll start by playing the flow, then the transition
+        ###statistic with mod7, week
+        flow_sample = pd.read_csv(sample_data_path + sample)
 
-    # #group by mod30, month
-    # sample_result_mod_30 = stat_mod_n(30, flow_sample)
+        ##sample
+        #group by mod7
+        sample_result_mod_7 = stat_mod_n(7, flow_sample)
+        # print(result_sample_by_mod7)
 
-    ##total
-    #group by mod7
-    total_result_mod_7 = stat_mod_n(7, flow_train)
+        # #group by mod30, month
+        # sample_result_mod_30 = stat_mod_n(30, flow_sample)
 
-    # #group by mod30, month
-    # total_result_mod_30 = stat_mod_n(30, flow_train)
+        ##total
+        #group by mod7
+        total_result_mod_7 = stat_mod_n(7, flow_train)
 
-    # #prediction of the coming 15 days based on mod7 stat
-    columns = ['date_dt', 'city_code', 'district_code', 'dwell', 'flow_in', 'flow_out']
-    flow_sample_prediction = pd.DataFrame(columns = columns)
-    for d in range(15):
-        day = 20180302 + d
-        dwell = sample_result_mod_7[(274 + d) % 7]['dwell'] * 0.9 + total_result_mod_7[(274 + d) % 7]['dwell'] * 0.1
-        flow_in = sample_result_mod_7[(274 + d) % 7]['flow_in'] * 0.9 + total_result_mod_7[(274 + d) % 7]['flow_in'] * 0.1
-        flow_out = sample_result_mod_7[(274 + d) % 7]['flow_out'] * 0.9 + total_result_mod_7[(274 + d) % 7]['flow_out'] * 0.1
-        flow_sample_prediction.loc[d] = {columns[0]:str(day),
-                                         columns[1]:'06d86ef037e4bd311b94467c3320ff38',
-                                         columns[2]:'85792b2278de59316d1158f6a97537ec',
-                                         columns[3]:dwell,
-                                         columns[4]:flow_in,
-                                         columns[5]:flow_out}
+        # #group by mod30, month
+        # total_result_mod_30 = stat_mod_n(30, flow_train)
 
-    print(flow_sample_prediction)
+        # #prediction of the coming 15 days based on mod7 stat
+        columns = ['date_dt', 'city_code', 'district_code', 'dwell', 'flow_in', 'flow_out']
+        flow_sample_prediction = pd.DataFrame(columns = columns)
+        for d in range(15):
+            day = 20180302 + d
+            dwell = sample_result_mod_7[(274 + d) % 7]['dwell'] * 0.9 + total_result_mod_7[(274 + d) % 7]['dwell'] * 0.1
+            flow_in = sample_result_mod_7[(274 + d) % 7]['flow_in'] * 0.9 + total_result_mod_7[(274 + d) % 7]['flow_in'] * 0.1
+            flow_out = sample_result_mod_7[(274 + d) % 7]['flow_out'] * 0.9 + total_result_mod_7[(274 + d) % 7]['flow_out'] * 0.1
+            flow_sample_prediction.loc[d] = {columns[0]:day,
+                                            columns[1]:city,
+                                            columns[2]:district,
+                                            columns[3]:dwell,
+                                            columns[4]:flow_in,
+                                            columns[5]:flow_out}
+
+        result_for_each_sample.append(flow_sample_prediction)
+
+    result = pd.concat(result_for_each_sample)
+    result.to_csv('./result/prediction.csv', index=False, header=None)
