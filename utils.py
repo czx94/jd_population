489b6ab31aa86c1026cc6bb5e7b9356927291741
caizixiang <czxplp@gmail.com>
2018-11-30 18:02:11 +0800

    Reconstruction

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 from sklearn.metrics import mean_squared_error, mean_squared_log_error, mean_absolute_error
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import pandas as pd
 
 def MSE(y1, y2):
     return mean_squared_error(y1, y2)
@@ -26,3 +31,27 @@ def eval(pred, groundtruth):
     result = RMSLE(predict, real)
     print('RMSLE error:', result)
     return result
+
+def predict_by_ARIMA(data, type, param=(1, 1, 6), offset = 1):
+    trend, seasonal, residual = decomp(data[type])
+
+    trend.dropna(inplace=True)
+
+    trend_model = ARIMA(trend, order=param).fit(disp=-1, method='css')
+    n = 15
+    trend_pred = trend_model.forecast(n)[0]
+    season_part = seasonal[offset:n+offset]
+    predict = pd.Series(trend_pred, index=season_part.index, name='predict')
+
+    return predict + season_part
+
+def decomp(data):
+    decomposition = seasonal_decompose(data, freq=7, two_sided=False)
+
+    trend = decomposition.trend
+
+    seasonal = decomposition.seasonal
+
+    residual = decomposition.resid
+
+    return trend, seasonal, residual
